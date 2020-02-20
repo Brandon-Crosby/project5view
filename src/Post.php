31f7bbf0d80b1c086e3f7ce11ed5597de5410629
Brandon-Crosby <53662951+Brandon-Crosby@users.noreply.github.com>
2020-02-20 14:56:08 -0500

    Add files via upload

--- src/Post.php
+++ src/Post.php
@@ -17,9 +17,6 @@ class Post
         $results->bindParam('id', $id);
         $results->execute();
         $post = $results->fetch();
-        //if (empty($Posts)) {
-            //throw new ApiException(ApiException::post_NOT_FOUND, 404);
-      //}
         return $post;
     }
     public function getPosts()
@@ -30,9 +27,6 @@ class Post
         //$results->bindParam('id', $post_id);
         $results->execute();
         return $results->fetchAll(PDO :: FETCH_ASSOC);
-        //if (empty($post)) {
-            //throw new ApiException(ApiException::post_NOT_FOUND, 404);
-        //}
         return $post;
     }
     //Create Journal Entry
@@ -45,25 +39,20 @@ class Post
         $results->execute();
         return true;
     }
-    public function updatePost($id,$title,$date, $body)
+    public function updatePost($id, $title, $date, $body)
     {
-        if (empty($data['Post_id']) || empty($data['title']) || empty($data['url'])) {
-          //  throw new ApiException(ApiException::Post_INFO_REQUIRED);
-        }
+        //if (empty($data['Post_id']) || empty($data['title']) || empty($data['url'])        }
         $results = $this->database->prepare(
-            'UPDATE posts SET title=:title, date = :date, body=:body WHERE id=:id'
-        );
+            'UPDATE posts SET title=:title, date = :date, body=:body WHERE id=:id');
         $results->bindParam(':id', $data, PDO::PARAM_INT);
         $results->bindParam(':title', $title, PDO::PARAM_STR);
         $results->bindParam(':date', $date, PDO::PARAM_STR);
         $results->bindParam(':body', $body, PDO::PARAM_STR);
         $results->execute();
-        //if ($results->rowCount()<1) {
-            //throw new ApiException(ApiException::Post_UPDATE_FAILED);
-        //}
-        return $this->get_Post($data['course_id']);
+        //return $this->get_Post($data['course_id']);
+        return true;
     }
-    public function delete_Post($Post_id)
+  /*  public function delete_Post($Post_id)
     {
         $this->getPosts($Post_id);
         $results = $this->database->prepare(
@@ -71,9 +60,7 @@ class Post
         );
         $results->bindParam('id', $Post_id);
         $results->execute();
-        //if ($statement->rowCount()<1) {
-            //throw new ApiException(ApiException::Post_DELETE_FAILED);
-        //}
         return ['message' => 'The post was deleted'];
     }
 }
+*/
